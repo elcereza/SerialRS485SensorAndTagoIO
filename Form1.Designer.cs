27c89a131a52416bdef4bf5c62bd68ef90ea1f8a
Gustavo Cereza <113127758+GustavoCereza@users.noreply.github.com>
2024-07-05 20:30:23 -0300

    new file:   .vs/ProjectEvaluation/sensorsrs485.metadata.v7.bin 	new file:   .vs/ProjectEvaluation/sensorsrs485.projects.v7.bin 	new file:   .vs/SensorsRS485/DesignTimeBuild/.dtbcache.v2 	new file:   .vs/SensorsRS485/FileContentIndex/a590edab-4a1e-40dd-b45a-aa14b08345c5.vsidx 	new file:   .vs/SensorsRS485/v17/.futdcache.v2 	new file:   .vs/SensorsRS485/v17/.suo 	new file:   Form1.Designer.cs 	new file:   Form1.cs 	new file:   Form1.resx 	new file:   Program.cs 	new file:   SensorsRS485.csproj 	new file:   SensorsRS485.csproj.user 	new file:   SensorsRS485.sln 	new file:   bin/Debug/net8.0-windows/SensorsRS485.deps.json 	new file:   bin/Debug/net8.0-windows/SensorsRS485.dll 	new file:   bin/Debug/net8.0-windows/SensorsRS485.exe 	new file:   bin/Debug/net8.0-windows/SensorsRS485.pdb 	new file:   bin/Debug/net8.0-windows/SensorsRS485.runtimeconfig.json 	new file:   bin/Debug/net8.0-windows/System.IO.Ports.dll 	new file:   bin/Debug/net8.0-windows/runtimes/linux-arm/native/libSystem.IO.Ports.Native.so 	new file:   bin/Debug/net8.0-windows/runtimes/linux-arm64/native/libSystem.IO.Ports.Native.so 	new file:   bin/Debug/net8.0-windows/runtimes/linux-x64/native/libSystem.IO.Ports.Native.so 	new file:   bin/Debug/net8.0-windows/runtimes/osx-arm64/native/libSystem.IO.Ports.Native.dylib 	new file:   bin/Debug/net8.0-windows/runtimes/osx-x64/native/libSystem.IO.Ports.Native.dylib 	new file:   bin/Debug/net8.0-windows/runtimes/unix/lib/net8.0/System.IO.Ports.dll 	new file:   bin/Debug/net8.0-windows/runtimes/win/lib/net8.0/System.IO.Ports.dll 	new file:   obj/Debug/net8.0-windows/.NETCoreApp,Version=v8.0.AssemblyAttributes.cs 	new file:   obj/Debug/net8.0-windows/SensorsRS485.AssemblyInfo.cs 	new file:   obj/Debug/net8.0-windows/SensorsRS485.AssemblyInfoInputs.cache 	new file:   obj/Debug/net8.0-windows/SensorsRS485.Form1.resources 	new file:   obj/Debug/net8.0-windows/SensorsRS485.GeneratedMSBuildEditorConfig.editorconfig 	new file:   obj/Debug/net8.0-windows/SensorsRS485.GlobalUsings.g.cs 	new file:   obj/Debug/net8.0-windows/SensorsRS485.assets.cache 	new file:   obj/Debug/net8.0-windows/SensorsRS485.csproj.AssemblyReference.cache 	new file:   obj/Debug/net8.0-windows/SensorsRS485.csproj.BuildWithSkipAnalyzers 	new file:   obj/Debug/net8.0-windows/SensorsRS485.csproj.CopyComplete 	new file:   obj/Debug/net8.0-windows/SensorsRS485.csproj.CoreCompileInputs.cache 	new file:   obj/Debug/net8.0-windows/SensorsRS485.csproj.FileListAbsolute.txt 	new file:   obj/Debug/net8.0-windows/SensorsRS485.csproj.GenerateResource.cache 	new file:   obj/Debug/net8.0-windows/SensorsRS485.designer.deps.json 	new file:   obj/Debug/net8.0-windows/SensorsRS485.designer.runtimeconfig.json 	new file:   obj/Debug/net8.0-windows/SensorsRS485.dll 	new file:   obj/Debug/net8.0-windows/SensorsRS485.genruntimeconfig.cache 	new file:   obj/Debug/net8.0-windows/SensorsRS485.pdb 	new file:   obj/Debug/net8.0-windows/apphost.exe 	new file:   obj/Debug/net8.0-windows/ref/SensorsRS485.dll 	new file:   obj/Debug/net8.0-windows/refint/SensorsRS485.dll 	new file:   obj/SensorsRS485.csproj.nuget.dgspec.json 	new file:   obj/SensorsRS485.csproj.nuget.g.props 	new file:   obj/SensorsRS485.csproj.nuget.g.targets 	new file:   obj/project.assets.json 	new file:   obj/project.nuget.cache

---
+++ Form1.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace SensorsRS485
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            textBox1 = new TextBox();
+            timer1 = new System.Windows.Forms.Timer(components);
+            tableLayoutPanel1 = new TableLayoutPanel();
+            panel6 = new Panel();
+            panel5 = new Panel();
+            id2_temperatureLabel = new Label();
+            panel4 = new Panel();
+            id2_humidityLabel = new Label();
+            panel3 = new Panel();
+            id1_conductivityLabel = new Label();
+            panel2 = new Panel();
+            id1_temperatureLabel = new Label();
+            panel1 = new Panel();
+            id1_humidityLabel = new Label();
+            timer2 = new System.Windows.Forms.Timer(components);
+            panel7 = new Panel();
+            id3_humidityLabel = new Label();
+            tableLayoutPanel1.SuspendLayout();
+            panel6.SuspendLayout();
+            panel5.SuspendLayout();
+            panel4.SuspendLayout();
+            panel3.SuspendLayout();
+            panel2.SuspendLayout();
+            panel1.SuspendLayout();
+            panel7.SuspendLayout();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.InfoText;
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Dock = DockStyle.Bottom;
+            textBox1.Font = new Font("Segoe UI", 14F);
+            textBox1.ForeColor = SystemColors.HighlightText;
+            textBox1.Location = new Point(0, 423);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.ScrollBars = ScrollBars.Vertical;
+            textBox1.Size = new Size(1327, 325);
+            textBox1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Interval = 5000;
+            timer1.Tick += timer1_Tick;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 3;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanel1.Controls.Add(panel6, 2, 1);
+            tableLayoutPanel1.Controls.Add(panel5, 1, 1);
+            tableLayoutPanel1.Controls.Add(panel4, 0, 1);
+            tableLayoutPanel1.Controls.Add(panel3, 2, 0);
+            tableLayoutPanel1.Controls.Add(panel2, 1, 0);
+            tableLayoutPanel1.Controls.Add(panel1, 0, 0);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 2;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Size = new Size(1327, 423);
+            tableLayoutPanel1.TabIndex = 1;
+            // 
+            // panel6
+            // 
+            panel6.Controls.Add(panel7);
+            panel6.Dock = DockStyle.Fill;
+            panel6.Location = new Point(887, 214);
+            panel6.Name = "panel6";
+            panel6.Size = new Size(437, 206);
+            panel6.TabIndex = 5;
+            // 
+            // panel5
+            // 
+            panel5.Controls.Add(id2_temperatureLabel);
+            panel5.Dock = DockStyle.Fill;
+            panel5.Location = new Point(445, 214);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(436, 206);
+            panel5.TabIndex = 4;
+            // 
+            // id2_temperatureLabel
+            // 
+            id2_temperatureLabel.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            id2_temperatureLabel.AutoSize = true;
+            id2_temperatureLabel.BackColor = Color.Transparent;
+            id2_temperatureLabel.Font = new Font("Segoe UI", 50F, FontStyle.Bold);
+            id2_temperatureLabel.ForeColor = SystemColors.ButtonHighlight;
+            id2_temperatureLabel.Location = new Point(86, 40);
+            id2_temperatureLabel.Name = "id2_temperatureLabel";
+            id2_temperatureLabel.Size = new Size(267, 112);
+            id2_temperatureLabel.TabIndex = 2;
+            id2_temperatureLabel.Text = "-00ºC";
+            id2_temperatureLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // panel4
+            // 
+            panel4.Controls.Add(id2_humidityLabel);
+            panel4.Dock = DockStyle.Fill;
+            panel4.Location = new Point(3, 214);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(436, 206);
+            panel4.TabIndex = 3;
+            // 
+            // id2_humidityLabel
+            // 
+            id2_humidityLabel.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            id2_humidityLabel.AutoSize = true;
+            id2_humidityLabel.BackColor = Color.Transparent;
+            id2_humidityLabel.Font = new Font("Segoe UI", 50F, FontStyle.Bold);
+            id2_humidityLabel.ForeColor = SystemColors.ButtonHighlight;
+            id2_humidityLabel.Location = new Point(86, 40);
+            id2_humidityLabel.Name = "id2_humidityLabel";
+            id2_humidityLabel.Size = new Size(264, 112);
+            id2_humidityLabel.TabIndex = 1;
+            id2_humidityLabel.Text = "000%";
+            id2_humidityLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(id1_conductivityLabel);
+            panel3.Dock = DockStyle.Fill;
+            panel3.Location = new Point(887, 3);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(437, 205);
+            panel3.TabIndex = 2;
+            // 
+            // id1_conductivityLabel
+            // 
+            id1_conductivityLabel.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            id1_conductivityLabel.AutoSize = true;
+            id1_conductivityLabel.BackColor = Color.Transparent;
+            id1_conductivityLabel.Font = new Font("Segoe UI", 50F, FontStyle.Bold);
+            id1_conductivityLabel.ForeColor = SystemColors.ButtonHighlight;
+            id1_conductivityLabel.Location = new Point(63, 46);
+            id1_conductivityLabel.Name = "id1_conductivityLabel";
+            id1_conductivityLabel.Size = new Size(327, 112);
+            id1_conductivityLabel.TabIndex = 1;
+            id1_conductivityLabel.Text = "0000us";
+            id1_conductivityLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(id1_temperatureLabel);
+            panel2.Dock = DockStyle.Fill;
+            panel2.Location = new Point(445, 3);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(436, 205);
+            panel2.TabIndex = 1;
+            // 
+            // id1_temperatureLabel
+            // 
+            id1_temperatureLabel.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            id1_temperatureLabel.AutoSize = true;
+            id1_temperatureLabel.BackColor = Color.Transparent;
+            id1_temperatureLabel.Font = new Font("Segoe UI", 50F, FontStyle.Bold);
+            id1_temperatureLabel.ForeColor = SystemColors.ButtonHighlight;
+            id1_temperatureLabel.Location = new Point(85, 46);
+            id1_temperatureLabel.Name = "id1_temperatureLabel";
+            id1_temperatureLabel.Size = new Size(267, 112);
+            id1_temperatureLabel.TabIndex = 1;
+            id1_temperatureLabel.Text = "-00ºC";
+            id1_temperatureLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(id1_humidityLabel);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(3, 3);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(436, 205);
+            panel1.TabIndex = 0;
+            // 
+            // id1_humidityLabel
+            // 
+            id1_humidityLabel.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            id1_humidityLabel.AutoSize = true;
+            id1_humidityLabel.BackColor = Color.Transparent;
+            id1_humidityLabel.Font = new Font("Segoe UI", 50F, FontStyle.Bold);
+            id1_humidityLabel.ForeColor = SystemColors.ButtonHighlight;
+            id1_humidityLabel.Location = new Point(87, 51);
+            id1_humidityLabel.Name = "id1_humidityLabel";
+            id1_humidityLabel.Size = new Size(264, 112);
+            id1_humidityLabel.TabIndex = 0;
+            id1_humidityLabel.Text = "000%";
+            id1_humidityLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // timer2
+            // 
+            timer2.Enabled = true;
+            timer2.Interval = 30000;
+            timer2.Tick += timer2_Tick;
+            // 
+            // panel7
+            // 
+            panel7.Controls.Add(id3_humidityLabel);
+            panel7.Dock = DockStyle.Fill;
+            panel7.Location = new Point(0, 0);
+            panel7.Name = "panel7";
+            panel7.Size = new Size(437, 206);
+            panel7.TabIndex = 4;
+            // 
+            // id3_humidityLabel
+            // 
+            id3_humidityLabel.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            id3_humidityLabel.AutoSize = true;
+            id3_humidityLabel.BackColor = Color.Transparent;
+            id3_humidityLabel.Font = new Font("Segoe UI", 50F, FontStyle.Bold);
+            id3_humidityLabel.ForeColor = SystemColors.ButtonHighlight;
+            id3_humidityLabel.Location = new Point(86, 40);
+            id3_humidityLabel.Name = "id3_humidityLabel";
+            id3_humidityLabel.Size = new Size(264, 112);
+            id3_humidityLabel.TabIndex = 1;
+            id3_humidityLabel.Text = "000%";
+            id3_humidityLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoScroll = true;
+            BackColor = SystemColors.ActiveCaptionText;
+            ClientSize = new Size(1327, 748);
+            Controls.Add(tableLayoutPanel1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            tableLayoutPanel1.ResumeLayout(false);
+            panel6.ResumeLayout(false);
+            panel5.ResumeLayout(false);
+            panel5.PerformLayout();
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel7.ResumeLayout(false);
+            panel7.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+        private TableLayoutPanel tableLayoutPanel1;
+        private Panel panel1;
+        private Panel panel6;
+        private Panel panel5;
+        private Panel panel4;
+        private Panel panel3;
+        private Panel panel2;
+        private Label id1_humidityLabel;
+        private Label id1_temperatureLabel;
+        private Label id2_temperatureLabel;
+        private Label id2_humidityLabel;
+        private Label id1_conductivityLabel;
+        private System.Windows.Forms.Timer timer2;
+        private Panel panel7;
+        private Label id3_humidityLabel;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.IO.Ports;
+using System.Text;
+
+namespace SensorsRS485
+{
+    public partial class Form1 : Form
+    {
+
+        byte[] buffer = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+
+        float[] temperature = { 0, 0, 0 };
+        float[] humidity = { 0, 0, 0 };
+        ushort conductivity = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void formmatter(bool tx_or_rx, string val)
+        {
+            string date = DateTime.Now.ToString("HH:mm:ss.fffffff ->");
+            if (tx_or_rx)
+                textBox1.Text += $"{date} TX:  {val}\r\n";
+            else
+                textBox1.Text += $"{date} RX:  {val}\r\n";
+        }
+
+        private byte[] CRC16(byte[] bytes, byte len)
+        {
+            ushort crc = 0xFFFF;
+            for (byte i = 0; i < len; ++i)
+            {
+                crc = (ushort)((crc & 0xFF00) | (crc & 0x00FF) ^ (bytes[i] & 0xFF));
+                for (byte j = 0; j < 8; j++)
+                {
+                    if ((crc & 0x0001) > 0)
+                    {
+                        crc = (ushort)(crc >> 1);
+                        crc = (ushort)(crc ^ 0xA001);
+                    }
+                    else
+                        crc = (ushort)(crc >> 1);
+                }
+            }
+            return new byte[2] { (byte)(crc % 0x100), (byte)(crc / 0x100) };
+        }
+
+        private void sendCommand(byte id, byte cmd)
+        {
+            SerialPort rs485 = new SerialPort();
+            rs485.PortName = "COM14";
+            rs485.BaudRate = 4800;
+            rs485.Parity = Parity.None;
+            rs485.StopBits = StopBits.One;
+            rs485.DataBits = 8;
+
+            // humidade:    04 03 00 00 00 01 84 5f      03 03 00 00 00 01 85 e8      02 03 00 00 00 01 84 39      01 03 00 00 00 01 84 0a
+            // temperatura: 04 03 00 01 00 01 d5 9f      03 03 00 01 00 01 d4 28      02 03 00 01 00 01 d5 f9      01 03 00 01 00 01 d5 ca 
+            // conduti:     04 03 00 02 00 01 25 9f      03 03 00 02 00 01 24 28      02 03 00 02 00 01 25 f9      01 03 00 02 00 01 25 ca
+
+            byte[] packet = { id, 0x03, 0x00, cmd, 0x00, 0x01, 0x00, 0x00 };
+
+            byte[] CRC = { 0x00, 0x00 };
+            CRC = CRC16(packet, (byte)(packet.Length - 2));
+            packet[5 + 1] = CRC[0];
+            packet[5 + 2] = CRC[1];
+            try
+            {
+                if (!rs485.IsOpen)
+                {
+                    rs485.Open();
+
+                    rs485.Write(packet, 0, packet.Length);
+
+                    string buf = "";
+                    for (int i = 0; i < packet.Length; ++i)
+                        buf += packet[i].ToString("X2") + " ";
+                    formmatter(true, buf);
+
+
+                    Thread.Sleep(100);
+                    rs485.Read(buffer, 0, buffer.Length);
+
+                    buf = "";
+                    for (int i = 0; i < packet.Length - 1; ++i)
+                        buf += buffer[i].ToString("X2") + " ";
+                    formmatter(false, buf);
+
+                    rs485.Close();
+                }
+            }
+            catch (Exception err)
+            {
+                textBox1.Text = err.Message;
+            }
+            textBox1.SelectionStart = textBox1.Text.Length - 1;
+            textBox1.ScrollToCaret();
+        }
+
+        private void getValues()
+        {
+            sendCommand(1, 0);
+            ushort buff = (ushort)((buffer[3] << 8) & 0xFF00);
+            buff |= buffer[4];
+            humidity[0] = (float)buff / 10;
+
+            sendCommand(2, 0);
+            buff = (ushort)((buffer[3] << 8) & 0xFF00);
+            buff |= buffer[4];
+            humidity[1] = (float)buff / 10;
+
+            sendCommand(3, 0);
+            buff = (ushort)((buffer[3] << 8) & 0xFF00);
+            buff |= buffer[4];
+            humidity[2] = (float)buff / 10;
+
+            sendCommand(1, 1);
+            buff = (ushort)((buffer[3] << 8) & 0xFF00);
+            buff |= buffer[4];
+            temperature[0] = (float)buff / 10;
+            temperature[0] = (buff & 0x1000) == 1 ? ((float)6553.5 - temperature[0]) * -1 : temperature[0];
+
+            sendCommand(2, 1);
+            buff = (ushort)((buffer[3] << 8) & 0xFF00);
+            buff |= buffer[4];
+            temperature[1] = (float)buff / 10;
+            temperature[1] = (buff & 0x1000) == 1 ? ((float)6553.5 - temperature[1]) * -1 : temperature[1];
+
+            sendCommand(3, 1);
+            buff = (ushort)((buffer[3] << 8) & 0xFF00);
+            buff |= buffer[4];
+            temperature[2] = (float)buff / 10;
+            temperature[2] = (buff & 0x1000) == 1 ? ((float)6553.5 - temperature[1]) * -1 : temperature[1];
+
+            sendCommand(1, 2);
+            conductivity = (ushort)((buffer[3] << 8) & 0xFF00);
+            conductivity |= buffer[4];
+
+
+            id1_humidityLabel.Text = humidity[0] == 0 ? $"  {humidity[0]}%" : $"{humidity[0]}%";
+            id2_humidityLabel.Text = humidity[1] == 0 ? $"  {humidity[1]}%" : $"{humidity[1]}%";
+            id3_humidityLabel.Text = humidity[2] == 0 ? $"  {humidity[2]}%" : $"{humidity[2]}%";
+
+            id1_temperatureLabel.Text = temperature[0] == 0 ? $"  {temperature[0]}ºC" : $"{temperature[0]}ºC";
+            id2_temperatureLabel.Text = temperature[1] == 0 ? $"  {temperature[1]}ºC" : $"{temperature[1]}ºC";
+
+            id1_conductivityLabel.Text = $"{conductivity}us";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Thread.Sleep(5000);
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            getValues();
+        }
+
+        async private void TagoIO(byte id) {
+            string deviceToken = id == 1 ? "token1" : "token2";
+            string tagoIOUrl = $"https://api.tago.io/data?token={deviceToken}";
+
+            string json = id == 1 ? $"[ {{\"variable\":\"temperature\", \"value\":{Convert.ToString(temperature[0]).Replace(",", ".")}, \"unit\":\"ºC\"}}," +
+                                    $"{{\"variable\":\"humidity\", \"value\":{Convert.ToString(humidity[0]).Replace(",", ".")}, \"unit\":\"%\"}}," +
+                                    $"{{\"variable\":\"conductivity\", \"value\":{conductivity}, \"unit\":\"us/cm\" }}]" :
+                                    $"[ {{\"variable\":\"temperature\", \"value\":{Convert.ToString(temperature[1]).Replace(",", ".")}, \"unit\":\"ºC\"}}," +
+                                    $"{{\"variable\":\"humidity\", \"value\":{Convert.ToString(humidity[1]).Replace(",", ".")}, \"unit\":\"%\"}} ]";
+            //textBox1.Text += json;
+            using (var client = new HttpClient())
+            {
+                //var json = Newtonsoft.Json.JsonConvert.SerializeObject(dataToSend);
+                
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                var response = await client.PostAsync(tagoIOUrl, content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    //textBox1.Text += "Sucesso no envio";
+                }
+                else
+                {
+                    //textBox1.Text += $"Erro: {response.StatusCode}";
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    //textBox1.Text += responseContent;
+                }
+            }
+        }
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            //TagoIO(1);
+            Thread.Sleep(1000);
+            //TagoIO(2);
+        }
+    }
+}
